78b735aeb2c530fa7cc7acaf9a99c4cb4f6faafe
Thor Lin <Thor_Lin@amat.com>
2021-12-16 11:40:37 +0800

    init demo

--- SHA256_cryptocurrency/Form1.Designer.cs
+++ SHA256_cryptocurrency/Form1.Designer.cs
@@ -28,13 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGetSHA256 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.cboZeroHeader = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblUsedtime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetSHA256
+            // 
+            this.btnGetSHA256.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetSHA256.Location = new System.Drawing.Point(406, 9);
+            this.btnGetSHA256.Name = "btnGetSHA256";
+            this.btnGetSHA256.Size = new System.Drawing.Size(123, 32);
+            this.btnGetSHA256.TabIndex = 0;
+            this.btnGetSHA256.Text = "Get SHA 256";
+            this.btnGetSHA256.UseVisualStyleBackColor = true;
+            this.btnGetSHA256.Click += new System.EventHandler(this.btnGetSHA256_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 47);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 187);
+            this.textBox1.TabIndex = 1;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Enabled = false;
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(639, 9);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(138, 32);
+            this.txtInput.TabIndex = 2;
+            // 
+            // cboZeroHeader
+            // 
+            this.cboZeroHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboZeroHeader.FormattingEnabled = true;
+            this.cboZeroHeader.Location = new System.Drawing.Point(138, 11);
+            this.cboZeroHeader.Name = "cboZeroHeader";
+            this.cboZeroHeader.Size = new System.Drawing.Size(81, 28);
+            this.cboZeroHeader.TabIndex = 3;
+            this.cboZeroHeader.SelectedIndexChanged += new System.EventHandler(this.cboZeroHeader_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "多少0在前面";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(535, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Work proof:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(422, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "使用時間:";
+            // 
+            // lblUsedtime
+            // 
+            this.lblUsedtime.AutoSize = true;
+            this.lblUsedtime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsedtime.Location = new System.Drawing.Point(505, 237);
+            this.lblUsedtime.Name = "lblUsedtime";
+            this.lblUsedtime.Size = new System.Drawing.Size(30, 20);
+            this.lblUsedtime.TabIndex = 7;
+            this.lblUsedtime.Text = "ms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 266);
+            this.Controls.Add(this.lblUsedtime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboZeroHeader);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGetSHA256);
+            this.Name = "Form1";
+            this.Text = "Workproof generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGetSHA256;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ComboBox cboZeroHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblUsedtime;
     }
 }
 
--- SHA256_cryptocurrency/Form1.cs
+++ SHA256_cryptocurrency/Form1.cs
@@ -1,20 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace SHA256_cryptocurrency
 {
     public partial class Form1 : Form
     {
+        string header = "0";
+        BackgroundWorker backgroundworker;
+
         public Form1()
         {
             InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            backgroundworker = new BackgroundWorker();
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();//引用stopwatch物件
+            
+            backgroundworker.DoWork += (sender, e) => {
+                btnGetSHA256.Enabled = false;
+                SHA256 mySHA256 = SHA256.Create();
+                sw.Reset();//碼表歸零
+                sw.Start();//碼表開始計時
+                for (ulong ul = 0; ul < Math.Pow(2, 256); ul++)
+                {
+                    txtInput.Text = ul.ToString();
+
+                    byte[] byteArray = BitConverter.GetBytes(ul);
+                    string strbinhash = "";
+                    byte[] hashArray = mySHA256.ComputeHash(byteArray);
+                    for (int i = 0; i < hashArray.Length; i++)
+                        strbinhash += Convert.ToString(hashArray[i], 2);
+
+                    textBox1.Text = strbinhash;
+
+                    if (strbinhash.Substring(0, header.Length) == header)
+                    {
+                        sw.Stop();
+                        return;
+                    }
+                }
+            };
+
+            backgroundworker.RunWorkerCompleted += (sender, e) => {
+                btnGetSHA256.Enabled = true;
+                lblUsedtime.Text = sw.Elapsed.TotalMilliseconds.ToString() + "ms";
+            };
+
+            for (int i = 1; i < 256; i++)
+                cboZeroHeader.Items.Add(i);
+        }
+
+        private void btnGetSHA256_Click(object sender, EventArgs e)
+        {
+
+            backgroundworker.RunWorkerAsync();
+        }
+
+        private void cboZeroHeader_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            header = "";
+            for (int i = 0; i < ((ComboBox)sender).SelectedIndex + 1; i++)
+                header += "0";
         }
     }
 }
